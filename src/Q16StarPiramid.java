2bc0b9c05b245d4a369bc85268d31dd76f65c156
Seokbin Yoon <nyoongoon@naver.com>
2021-01-28 14:36:36 +0900

    Q16 별피라미드.Before

---
+++ src/Q16StarPiramid.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q16StarPiramid {
+    public static void main(String[] args){
+        Scanner stdIn = new Scanner(System.in);
+        int n = stdIn.nextInt();
+        spira(n);
+    }
+
+    static void spira(int n){
+        for(int i = 1; i<=n; i++){
+            for(int k=n-i; k>=1; k--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=(i-1)*2+1; j++){
+                System.out.print("*");
+            }
+            for(int k=n-i; k>=1; k--){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
