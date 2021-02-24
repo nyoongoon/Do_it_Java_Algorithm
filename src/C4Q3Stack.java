f633c81df3f7ccc24a4ed1dd4366cb80e5d80947
Seokbin Yoon <nyoongoon@naver.com>
2021-02-24 13:52:23 +0900

    C4Q3양방향스택(필드 초기화시 생성자 이용!)

--- src/C4Q3Stack.java
+++ src/C4Q3Stack.java
@@ -1,13 +1,15 @@
 public class C4Q3Stack {
     public class IntStack {
         private int max;
-        private int ptrA = 0;
-        private int ptrB = max-1;
+        private int ptrA;   //생성자로 초기화 하지 않으면 필드값 초기화 안됌!
+        private int ptrB;
         private int[] stk;
 
         //생성자
         public IntStack(int capacity) {
             max = capacity;
+            ptrA = 0;
+            ptrB = max - 1;
             try {
                 stk = new int[max]; // 스택 본체용 배열 생성
             } catch (OutOfMemoryError e) { //생성할 수 없음
